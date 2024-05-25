f326d33e5fa1b2de2d9da02a5f9a182742b9df81
Tomas Nemec <xnemect@gmail.com>
2024-05-25 23:39:07 +0200

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,10 +10,24 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:SURGEON_API_ENVIRONMENT="Development"
 $env:SURGEON_API_PORT="8080"
+$env:SURGEON_API_MONGODB_USERNAME="root"
+$env:SURGEON_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/surgeon-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/surgeon-api-service
+        } finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
